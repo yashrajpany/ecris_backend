46a66ca60a1641d069173d234f2af1680653adf5
yrp <yrpany@gmail.com>
2021-09-29 16:47:44 +0530

    added role authorization

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -11,24 +11,26 @@ const {
 
 const router = express.Router()
 
-const { protect } = require('../middleware/auth')
+const { protect, authorize } = require('../middleware/auth')
 
 const Bootcamp = require('../models/Bootcamp')
 const advancedResults = require('../middleware/advancedResults')
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 
-router.route('/:id/photo').put(protect, bootcampPhotoUpload)
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload)
 
 router
   .route('/')
   .get(advancedResults(Bootcamp), getBootcamps)
-  .post(protect, createBootcamp)
+  .post(protect, authorize('publisher', 'admin'), createBootcamp)
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp)
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp)
 
 module.exports = router
