cc9b5442773ce6bdf6589ae78ad18766274e9650
yrp <yrpany@gmail.com>
2021-09-30 12:04:39 +0530

    forgotpassword route added with controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,6 +48,39 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponce(user, 200, res)
 })
 
+// @desc Get logged in user
+// @route GET /api/v1/auth/me
+// @access Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id)
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  })
+})
+
+// @desc Forgot password
+// @route POST /api/v1/auth/forgotpassword
+// @access Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email })
+
+  if (!user) {
+    return next(new ErrorResponce('There is no user with that email', 404))
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken()
+
+  await user.save({ validateBeforeSave: false })
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  })
+})
+
 const sendTokenResponce = (user, statusCode, res) => {
   // Create token
   const token = user.getSignedJwtToken()
@@ -65,15 +98,3 @@ const sendTokenResponce = (user, statusCode, res) => {
     .cookie('token', token, options)
     .json({ success: true, token })
 }
-
-// @desc Get logged in user
-// @route GET /api/v1/auth/me
-// @access Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id)
-
-  res.status(200).json({
-    success: true,
-    data: user,
-  })
-})
