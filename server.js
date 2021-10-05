c8bb3804f1bb499406e1ce88a98bb47f82d151d2
yrp <yrpany@gmail.com>
2021-10-05 23:06:18 +0530

    changed route to events

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(fileupload())
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Mouting routers
-app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/events', bootcamps)
 app.use('/api/v1/auth', auth)
 app.use('/api/v1/users', users)
 app.use('/api/v1/reviews', reviews)
